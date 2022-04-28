9a3cddc3fb4fabf9e3b50f293e35c052e82881ee
Daniela T <94473375+Peneloppe-jack@users.noreply.github.com>
2022-04-28 15:37:42 +0200

    on and on

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -70,7 +70,7 @@ return (
 
 
 RegistrationView.propTypes = {
-  onRegistration: PropTypes.shape({
+  register: PropTypes.shape({
       Username: PropTypes.string.isRequired,
       Password: PropTypes.string.isRequired,
       Email: PropTypes.string.isRequired
